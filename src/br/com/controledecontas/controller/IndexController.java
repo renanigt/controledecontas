b25ed487a6846590fd950e9db6a7e3c8a5a95db6
Renan Montenegro <renanigt@gmail.com>
2012-07-25 17:01:27 -0300

    Criado classe IndexController e o jsp para teste do VRaptor.

---
+++ src/br/com/controledecontas/controller/IndexController.java
@@ -0,0 +1,21 @@
+package br.com.controledecontas.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+@Resource
+public class IndexController {
+
+	Result result;
+	
+	public IndexController(Result result) {
+		this.result = result;
+	}
+	
+	@Path("/")
+	public void teste() {
+		result.include("funcionando", "VRaptor está funcionando !");
+	}
+	
+}
