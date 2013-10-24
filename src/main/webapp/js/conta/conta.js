37e5d32d4fda33a2e6367519ea8494cdd1758925
Renan <renanigt@gmail.com>
2013-10-24 00:30:39 -0300

    Refactoring no JS de conta.

--- src/main/webapp/js/conta/conta.js
+++ src/main/webapp/js/conta/conta.js
@@ -1,47 +1,44 @@
 $(document).ready(function() {
 	
-	$(".close").each(function() {
-		$(this).click(function() {
-			$(this).parent().fadeOut("slow");
-		});
+	$(".close").click(function() {
+		$(this).parent().fadeOut("slosw");
 	});
 	
-	$(".acoes").each(function(index) {
-		$("#delete" + index).click(function() {
-			
-			var exclusao = confirm("Tem certeza que deseja excluir essa Conta ?");
-			
-			if(exclusao) {
-				$.ajax({
-					type: "GET",
-					url: $("#urlDelete" + index).val(),
-					success: function(json) {
-						var saldo = json.saldo;
-						var mensagem = json.mensagem;
-						
-						$("#urlDelete" + index).parent().parent().remove();
-						$("#saldo").html(saldo);
+	$(".acoes .delete").click(function(event) {
+		event.preventDefault();
+		
+		var exclusao = confirm("Tem certeza que deseja excluir essa Conta ?");
+		var conta = $(this).parent().parent();
+		
+		if(exclusao) {
+			$.ajax({
+				type: "GET",
+				url: $(this).attr("href"),
+				success: function(json) {
+					var saldo = json.saldo;
+					var mensagem = json.mensagem;
+					
+					conta.fadeOut();
+					$("#saldo").html(saldo);
 
-						$("#mensagemSucesso").html(mensagem);
-						$("#sucesso").fadeIn("slow");
+					$("#mensagemSucesso").html(mensagem);
+					$("#sucesso").fadeIn("slow");
 
-						if(saldo > 0) {
-							$("#saldo").css("color", "blue");
-						} else if(saldo < 0) {
-							$("#saldo").css("color", "red");
-						} else {
-							$("#saldo").css("color", "black");
-						}
-					},
-					error: function(json) {
-						var erro = $.parseJSON(json.responseText);
-						$("#mensagemErro").html(erro);
-						$("#erro").fadeIn("slow");
+					if(saldo > 0) {
+						$("#saldo").css("color", "blue");
+					} else if(saldo < 0) {
+						$("#saldo").css("color", "red");
+					} else {
+						$("#saldo").css("color", "black");
 					}
-				});
-			}
-			
-		});
+				},
+				error: function(json) {
+					var erro = $.parseJSON(json.responseText);
+					$("#mensagemErro").html(erro);
+					$("#erro").fadeIn("slow");
+				}
+			});
+		}
 	});
 	
 });
